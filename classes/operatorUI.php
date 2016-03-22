beff966bce56465f7b733558db4209855bc12bdd
Sam Barratt <samathy@sbarratt.co.uk>
2016-03-22 23:16:13 +0000

    Working on the UI - Initial html

--- classes/operatorUI.php
+++ classes/operatorUI.php
@@ -4,7 +4,32 @@ class operatorUI
 {
     public function showUI()    //Displays the task selection box
     {
-        echo"Pick an operation to perform";
+
+        echo '
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <h1>Operator Home</h1>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <h3>Pick an operation to perform</h3>";
+            </div>
+        </div>
+        ';
+        //Set the divs for the forms for the options.
+        echo '                  
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <p class="text-center"> Add</p>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <p class="text-center"> Amend</p>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <p class="text-center"> delete</p>
+            </div>
+        </div>
+        ';
+
     }
     public function pickTask($task)
     {
